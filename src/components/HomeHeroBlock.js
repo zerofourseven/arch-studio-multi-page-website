28341e7d08df667b13eee06baa1c935d0a6c27f9
zerofourseven <cj315@hotmail.com>
2021-12-30 16:35:53 -0500

    Implemented Home Hero slides block

--- src/components/HomeHeroBlock.js
+++ src/components/HomeHeroBlock.js
@@ -1,57 +1,94 @@
 import React from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { slideData } from '../data/data'
 import Button from './Button'
 
 const HomeHeroBlock = () => {
-    const delay = 2500;
+    const duration = 10000;
+    const [chosenSlide, setChosenSlide] = useState(0);
+    const timeoutRef = useRef(null);
 
-    //TODO setup auto-timer and also Desktop buttons for choosing slide
+    useEffect(() => {
+        if(timeoutRef.current) 
+            clearTimeout(timeoutRef.current);
 
+        timeoutRef.current = setTimeout(() => 
+            setChosenSlide((slide) => 
+                slide === slideData.length - 1 ? 0 : slide + 1
+            ), duration);
+
+
+        return () => {
+            if(timeoutRef.current) 
+                clearTimeout(timeoutRef.current);
+        };
+
+    }, [chosenSlide]);
 
     return (
         <section className='container block home-hero-block'>
-            <div>
-            {
-                slideData.map((project, index) => {
-                const { title, description, image} = project;
-                return (
+            <div className='home-hero-block__slideshow-container'>
+                <div className='home-hero-block__slideshow'>
+                {
+                    slideData.map((project, index) => {
+                    const { title, description, image} = project;
+                    let slideState = index === chosenSlide ? ' active' : '';
+                    /*if (index === chosenSlide - 1 || chosenSlide === 0 && index === slideData.length){
+                        slideState = ' active-previous';
+                    }*/
+                    return (
 
-                    <article className="home-hero-block__slide" key={index}>
-                        <div className='home-hero-block__image-container'>
-                            <picture>
-                                <source srcSet={image.desktop} media="(min-width: 1440px)" type="image/webp"/>
-                                <source srcSet={image.tablet} media="(min-width: 600px)" type="image/webp"/>
-                                <source srcSet={image.mobile} type="image/webp"/>
+                        <article className={`home-hero-block__slide${slideState}`} key={index}>
+                            <div className='home-hero-block__image-container'>
+                                <picture>
+                                    <source srcSet={image.desktop} media="(min-width: 1440px)" type="image/webp"/>
+                                    <source srcSet={image.tablet} media="(min-width: 600px)" type="image/webp"/>
+                                    <source srcSet={image.mobile} type="image/webp"/>
 
-                                <source srcSet={image.desktop} media="(min-width: 1440px)" type="image/jpeg"/>
-                                <source srcSet={image.tablet} media="(min-width: 600px)" type="image/jpeg"/>
-                                <source srcSet={image.mobile} type="image/jpeg"/>
-                                <img
-                                srcSet={`${image.mobile} 375w, ${image.tablet} 573w, ${image.desktop} 1110w`}
-                                src={image.mobile}
-                                alt={title}
-                                className="project__image"
-                                />
-                            </picture>
-                        </div>
+                                    <source srcSet={image.desktop} media="(min-width: 1440px)" type="image/jpeg"/>
+                                    <source srcSet={image.tablet} media="(min-width: 600px)" type="image/jpeg"/>
+                                    <source srcSet={image.mobile} type="image/jpeg"/>
+                                    <img
+                                    srcSet={`${image.mobile} 375w, ${image.tablet} 573w, ${image.desktop} 1110w`}
+                                    src={image.mobile}
+                                    alt={title}
+                                    className="project__image"
+                                    />
+                                </picture>
+                            </div>
 
-                        <div className="project__info">
-                            <h1>{title}</h1>
-                            <p>{description}</p>
-                            <Button btnClassname="home-hero-block__portfolio-button" linkName="/portfolio" text="see our portfolio" arrow="true" altText="View portfolio" />
-                        </div>
+                            <div className="project__info">
+                                <h1>{title}</h1>
+                                <p>{description}</p>
+                                <Button btnClassname="home-hero-block__portfolio-button" linkName="/portfolio" text="see our portfolio" arrow="true" altText="View portfolio" />
+                            </div>
+                            
                         
+                        </article>
                     
-                    </article>
-                 
-                );
+                    );
+                    }
+
+                    )
+
                 }
-
-                )
-
-            }
+                    
+                </div>
+                
             </div>
-            
+            <div className='home-hero-block__slideshow-buttons'>
+                {
+                    slideData.map((project, index) => {
+                        let buttonState = index === chosenSlide ? ' button--active' : '';
+                        return (
+                            <button key={index} className={`button${buttonState}`} onClick={() => {setChosenSlide(index);}}>{index + 1}</button>
+                        );
+                    }
+                    )
+
+                }
+                    
+                </div>
         </section>
     )
 }
