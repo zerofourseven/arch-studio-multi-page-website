062a38d846fb469ac79ec5baece98ece73b9f8cd
zerofourseven <cj315@hotmail.com>
2022-01-02 16:03:02 -0500

    Improved contact form

--- src/components/FormBlock.js
+++ src/components/FormBlock.js
@@ -1,25 +1,90 @@
 import React from 'react'
-import Button from './Button.js'
-import { useRef } from "react";
+import { useState, useRef } from "react";
+import arrowIcon from '../assets/icons/icon-arrow.svg';
 
+const FormBlock = () => {   
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [submitSuccess, setSubmitSuccess] = useState(false);
 
-const FormBlock = () => {
     const nameInputRef = useRef();
+    const emailInputRef = useRef();
+    const messageInputRef = useRef();
     
     function submitHandler(event) {
-        
+        event.preventDefault(); //lets me handle submission fully with JS & React
+        const enteredName = nameInputRef.current.value;
+        const enteredEmail = emailInputRef.current.value;
+        const enteredMessage = messageInputRef.current.value;
+
+        setName('');
+        setEmail('');
+        setMessage('');
+        setSubmitSuccess(true);
+
+        const messageData = {
+            name: enteredName,
+            email: enteredEmail,
+            message: enteredMessage
+        }
+
+        console.log(messageData);
+
+        //if connecting to some backend...
+       /* fetch(
+            "", //url to post the data to
+            {
+                method: "POST",
+                body: JSON.stringify(messageData),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );*/
     }
 
 
     return (
         <section className='container block form-block'>
             <h2 className='form-block__title'>Connect with us</h2>
-            <form className='fomr-block__form' onSubmit={submitHandler}>
-                <input type="text" required ref={nameInputRef} placeholder='Name' />
-                <input type="email" required  placeholder='Email' />
-                <textarea rows="4" required placeholder='Message ' />
-                <Button className='form-block__submit' btnClassname='button--block' arrow='true'/>
-            </form>
+            
+            {submitSuccess ? 
+                <div className='form-block__success-message'>
+                    Message sent! 
+                </div> 
+            :
+                <form className='form-block__form' onSubmit={submitHandler}>
+                    <input 
+                        className='form-block__form-name' 
+                        type="text" 
+                        value={name} 
+                        onChange={(e) => {setName(e.target.value)}}
+                        required 
+                        ref={nameInputRef} 
+                        placeholder='Name' /><span></span>
+                    <input 
+                        className='form-block__form-email' 
+                        type="email" 
+                        value={email} 
+                        onChange={(e) => {setEmail(e.target.value)}}
+                        required 
+                        ref={emailInputRef} 
+                        placeholder='Email' /><span></span>
+                    <textarea 
+                        className='form-block__form-message' 
+                        rows="4" 
+                        value={message} 
+                        onChange={(e) => {setMessage(e.target.value)}}
+                        required 
+                        ref={messageInputRef} 
+                        placeholder='Message' /><span></span>
+                    <button className='button button--block form-block__submit' type='submit'> 
+                        <img src={arrowIcon} alt='Submit form' className="button__arrow" />
+                    </button>
+                </form>
+                
+                }
         </section>
     )
 }
